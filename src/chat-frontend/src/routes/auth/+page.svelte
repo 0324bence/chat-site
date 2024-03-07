4d27f0ae0a6c3e9d9d90413da5e59ed412ea7587
0324bence <d.bence0324@gmail.com>
2024-03-07 12:46:49 +0100

    create login page

---
+++ src/chat-frontend/src/routes/auth/+page.svelte
@@ -0,0 +1,10 @@
+<div id="wrapper">
+    <form action="" id="login">
+        <h1>Login</h1>
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+        <p class="message">Új felhasználó: <a href="register">Regisztráció</a></p>
+    </form>
+</div>
+
