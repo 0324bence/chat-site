d4e2f95a87f90b6dffcd90d27d291b77aed29f07
timre13 <itorteli13@gmail.com>
2024-03-07 13:04:55 +0100

    Fix global path in Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-  
+    app.setGlobalPrefix("api");
+
     const config = new DocumentBuilder().setTitle("Chat Site API").setDescription("Chat Site API").build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("api", app, document);
-  
-    app.setGlobalPrefix("api");
+
     await app.listen(3000);
 }
 bootstrap();
