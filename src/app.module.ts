2f64abb3397db8609777e68890bd2cf20387d6b4
timre13 <itorteli13@gmail.com>
2024-03-14 12:39:43 +0100

    Use .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,24 @@ import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/user.entity";
 import { AuthModule } from "./auth/auth.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: "mysql",
-            host: "127.0.0.1",
-            port: 3306,
-            username: "user",
-            password: "pass",
-            database: "chat-site",
-            synchronize: true,
-            entities: [User]
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: "mysql",
+                host: configService.get<string>("DB_HOST"),
+                port: configService.get<number>("DB_PORT"),
+                username: configService.get<string>("DB_USER"),
+                password: configService.get<string>("DB_PASS"),
+                database: configService.get<string>("DB_TABLE"),
+                synchronize: true,
+                entities: [User]
+            }),
+            inject: [ConfigService]
         }),
         UsersModule,
         AuthModule
